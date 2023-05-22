cfe905420fb8e53f313bbaf9c624119b75f2d86c
pepepapi <samuelitogonzalitoampuerito@gmail.com>
2023-05-22 11:04:31 +0100

    Completed cockroach_speed.py

---
+++ python/cockroach_speed.py
@@ -0,0 +1,13 @@
+import math
+
+def cockroach_speed(s):
+    return math.floor(s / 0.036)
+
+print(cockroach_speed(1.08)) # * --> 30
+print(cockroach_speed(1.09)) # * --> 30
+
+# * SUCCESSFULLY COMPLETED WITHOUT UNLOCKING SOLUTION
+# * Completed Date: 21.05.2023
+# ? == BREADCOWN ======================================================================================== DESCRIPTION ====
+# ? 1. Round a the result of s divided by 0.036                                                         | MATH, FLOOR, INT
+
